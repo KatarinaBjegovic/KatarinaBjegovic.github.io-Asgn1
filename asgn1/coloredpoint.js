e980e152bdf91bf4705d08300c0cee973763bd0b
Katarina Bjegovic <kbjegovi@ucsc.edu>
2025-01-19 22:28:07 -0800

    final fix for winter

--- asgn1/coloredpoint.js
+++ asgn1/coloredpoint.js
@@ -72,150 +72,6 @@ class HardTriangle{
     }
 }
 
-
-
-function drawAllTriangles() {
-    let triangles = [
-        [0, 140, -40, 80, 40, 80], // top tree
-        [0, 100, -60, 0, 60, 0], // mid tree 
-        [0, 20, -80, -80, 80, -80], //bottom tree
-
-        [-20, -80, 20, -80, -20, -140], //L stump
-        [20, -80, -20, -140, 20, -140], // R stump
-
-        [100, 0, 120, 0, 100, -140], // ski 1 L
-        [120, 0, 100, -140, 120, -140], // ski 1 R
-        [140, 0, 160, 0, 140, -140], // ski 2 L
-        [160, 0, 140, -140, 160, -140] // ski 2 R
-    ]; 
-    let black_triangles = [
-        [0, 130, -30, 85, 30, 85], // top tree
-        [0, 90, -50, 5, 50, 5], // mid tree 
-        [0, 10, -70, -75, 70, -75], //bottom tree
-
-        [-15, -70, 15, -70, -15, -135], //L stump
-        [15, -70, -15, -135, 15, -135], // R stump
-
-        [105, -5, 115, -5, 105, -135], // ski 1 L
-        [115, -5, 105, -135, 115, -135], // ski 1 R
-        [145, -5, 155, -5, 145, -135], // ski 2 L
-        [155, -5, 145, -135, 155, -135] // ski 2 R
-    ]; 
-    let circles = [
-        [-10,110,0],
-        [10,70,0],
-        [-10,30,0],
-        [-50,-90,0],
-        [30,-70,0],
-        [10,-30,0],
-        [-10,-50,0]
-    ];
-
-    gl.clear(gl.COLOR_BUFFER_BIT); // Clear the canvas before drawing
-
-    for (let i = 0; i < triangles.length; i++) {
-        let t_coord = triangles[i];
-        t_coord = [t_coord[0]/200, t_coord[1]/150, t_coord[2]/150, t_coord[3]/150, t_coord[4]/150, t_coord[5]/150];
-        let white_tri = new HardTriangle();
-        white_tri.coors = t_coord;
-        white_tri.color = [1.0,1.0,1.0,1.0];
-        g_shapesList.push(white_tri);
-    }
-    for (let i = 0; i < black_triangles.length; i++) {
-        let t_coord = black_triangles[i];
-        t_coord = [t_coord[0]/200, t_coord[1]/150, t_coord[2]/150, t_coord[3]/150, t_coord[4]/150, t_coord[5]/150];
-        let black_tri = new HardTriangle();
-        black_tri.coors = t_coord;
-        black_tri.color = [0.0,0.0,0.0,1.0];
-        g_shapesList.push(black_tri);
-    }
-
-    gl.uniform4f(u_FragColor, 0.0, 0.0, 0.0, 1.0);
-    for (let i = 0; i < circles.length; i++) {
-        let c_coord = circles[i];
-        c_coord = [c_coord[0]/200, c_coord[1]/200, c_coord[2]/200];
-        let circ = new Circle();
-        circ.position = c_coord;
-        circ.size = 10.0;
-        circ.color = [1.0,0.0,0.0,1.0];
-        circ.segments = 20;
-        g_shapesList.push(circ);
-        let circ_black = new Circle();
-        circ_black.position = c_coord;
-        circ_black.size = 8.0;
-        circ_black.color = [0.0,0.0,0.0,1.0];
-        circ_black.segments = 20;
-        g_shapesList.push(circ_black);
-    }
-
-    renderAllShapes();
-
-
-
-}
-
-
-
-// function drawAllTriangles() {
-//     let triangles = [
-//         [0, 140, -40, 80, 40, 80], // top tree
-//         [0, 100, -60, 0, 60, 0], // mid tree 
-//         [0, 20, -80, -80, 80, -80], //bottom tree
-
-//         [-20, -80, 20, -80, -20, -140], //L stump
-//         [20, -80, -20, -140, 20, -140], // R stump
-
-//         [100, 0, 120, 0, 100, -140], // ski 1 L
-//         [120, 0, 100, -140, 120, -140], // ski 1 R
-//         [140, 0, 160, 0, 140, -140], // ski 2 L
-//         [160, 0, 140, -140, 160, -140] // ski 2 R
-//     ]; 
-//     let black_triangles = [
-//         [0, 130, -30, 85, 30, 85], // top tree
-//         [0, 90, -50, 5, 50, 5], // mid tree 
-//         [0, 10, -70, -75, 70, -75], //bottom tree
-
-//         [-15, -70, 15, -70, -15, -135], //L stump
-//         [15, -70, -15, -135, 15, -135], // R stump
-
-//         [105, -5, 115, -5, 105, -135], // ski 1 L
-//         [115, -5, 105, -135, 115, -135], // ski 1 R
-//         [145, -5, 155, -5, 145, -135], // ski 2 L
-//         [155, -5, 145, -135, 155, -135] // ski 2 R
-//     ]; 
-
-//     gl.clear(gl.COLOR_BUFFER_BIT); // Clear the canvas before drawing
-
-//     gl.uniform4f(u_FragColor, 1.0, 1.0, 1.0, 1.0); // Set a default color for the triangles
-//     for (let i = 0; i < triangles.length; i++) {
-//         console.log("Drawing triangle ", i + 1, " with vertices: ", triangles[i]);
-//         let t = triangles[i];
-//         console.log("Drawing triangle ", i + 1, " with vertices: ", t[0]);
-//         drawTriangle([t[0]/200, t[1]/200, t[2]/200, t[3]/200, t[4]/200, t[5]/200]);
-//         //drawTriangle([(t[0] + 200)/200, (-1*t[1]+200)/200, (t[2]+200)/200, (-1*t[3]+200)/200, (t[4]+200)/200, (-1*t[5]+200)/200]);
-//     }
-
-//     gl.uniform4f(u_FragColor, 0.0, 0.0, 0.0, 1.0);
-//     for (let i = 0; i < triangles.length; i++) {
-//         let t = black_triangles[i];
-//         drawTriangle([t[0]/200, t[1]/200, t[2]/200, t[3]/200, t[4]/200, t[5]/200]);
-//         //drawTriangle([(t[0] + 200)/200, (-1*t[1]+200)/200, (t[2]+200)/200, (-1*t[3]+200)/200, (t[4]+200)/200, (-1*t[5]+200)/200]);
-//     }
-
-//     gl.uniform4f(u_FragColor, 0.0, 0.0, 0.0, 1.0);
-//     for (let i = 0; i < triangles.length; i++) {
-//         let t = black_triangles[i];
-//         drawTriangle([t[0]/200, t[1]/200, t[2]/200, t[3]/200, t[4]/200, t[5]/200]);
-//         //drawTriangle([(t[0] + 200)/200, (-1*t[1]+200)/200, (t[2]+200)/200, (-1*t[3]+200)/200, (t[4]+200)/200, (-1*t[5]+200)/200]);
-//     }
-
-
-
-
-
-// }
-
-
 class Triangle{
     constructor(){
         this.type = 'triangle';
@@ -283,7 +139,8 @@ let g_selectedColor = [0.5,0.5,0.5,1.0];
 let g_selectedSize = 20.0;
 let g_selectedType=SQUARE;
 let g_selectedSegments = 10.0;
-var g_shapesList = []
+var g_shapesList = [];
+var g_shapesList_WINTER = [];
 
 
 function setupWebGL(){
@@ -332,7 +189,7 @@ function addActionsForHTMLUI(){
     document.getElementById('triangle').onclick = function() {g_selectedType=TRIANGLE};
     document.getElementById('circle').onclick = function() {g_selectedType=CIRCLE};
 
-    document.getElementById('pix_winter').onclick = drawAllTriangles;
+    document.getElementById('pix_winter').onclick = WinterPage;
 
 }
 
@@ -399,3 +256,79 @@ function click(ev ) {
 }
 
  
+
+
+function WinterPage() {
+    let triangles = [
+        [0, 140, -40, 80, 40, 80], // top tree
+        [0, 100, -60, 0, 60, 0], // mid tree 
+        [0, 20, -80, -80, 80, -80], //bottom tree
+
+        [-20, -80, 20, -80, -20, -140], //L stump
+        [20, -80, -20, -140, 20, -140], // R stump
+
+        [100, 0, 120, 0, 100, -140], // ski 1 L
+        [120, 0, 100, -140, 120, -140], // ski 1 R
+        [140, 0, 160, 0, 140, -140], // ski 2 L
+        [160, 0, 140, -140, 160, -140] // ski 2 R
+    ]; 
+    let black_triangles = [
+        [0, 130, -30, 85, 30, 85], // top tree
+        [0, 90, -50, 5, 50, 5], // mid tree 
+        [0, 10, -70, -75, 70, -75], //bottom tree
+
+        [-15, -70, 15, -70, -15, -135], //L stump
+        [15, -70, -15, -135, 15, -135], // R stump
+
+        [105, -5, 115, -5, 105, -135], // ski 1 L
+        [115, -5, 105, -135, 115, -135], // ski 1 R
+        [145, -5, 155, -5, 145, -135], // ski 2 L
+        [155, -5, 145, -135, 155, -135] // ski 2 R
+    ]; 
+    let circles = [
+        [-10,110,0],
+        [10,70,0],
+        [-10,30,0],
+        [-50,-90,0],
+        [30,-70,0],
+        [10,-30,0],
+        [-10,-50,0]
+    ];
+
+    gl.clear(gl.COLOR_BUFFER_BIT); // Clear the canvas before drawing
+
+    for (let i = 0; i < triangles.length; i++) {
+        let t_coord = triangles[i];
+        t_coord = [t_coord[0]/150, t_coord[1]/150, t_coord[2]/150, t_coord[3]/150, t_coord[4]/150, t_coord[5]/150];
+        let white_tri = new HardTriangle();
+        white_tri.coors = t_coord;
+        white_tri.color = [1.0,1.0,1.0,1.0];
+        g_shapesList.push(white_tri);
+    }
+    for (let i = 0; i < black_triangles.length; i++) {
+        let t_coord = black_triangles[i];
+        t_coord = [t_coord[0]/150, t_coord[1]/150, t_coord[2]/150, t_coord[3]/150, t_coord[4]/150, t_coord[5]/150];
+        let black_tri = new HardTriangle();
+        black_tri.coors = t_coord;
+        black_tri.color = [0.0,0.0,0.0,1.0];
+        g_shapesList.push(black_tri);
+    }
+    gl.uniform4f(u_FragColor, 0.0, 0.0, 0.0, 1.0);
+    for (let i = 0; i < circles.length; i++) {
+        let c_coord = circles[i];
+        c_coord = [c_coord[0]/150, c_coord[1]/150, c_coord[2]/150];
+        let circ = new Circle();
+        circ.position = c_coord;
+        circ.size = 10.0;
+        circ.color = [1.0,1.0,1.0,1.0];
+        circ.segments = 20;
+        g_shapesList.push(circ);
+        let circ_black = new Circle();
+        circ_black.position = c_coord;
+        circ_black.size = 8.0;
+        circ_black.color = [0.0,0.0,0.0,1.0];
+        circ_black.segments = 20;
+        g_shapesList.push(circ_black);
+    }
+    renderAllShapes();
+}
